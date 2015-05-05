ece61cf860fcb3a4c72e7a9d6e92915295f8cda4
sbastian <sbastian@kil-m-34.htw-saarland.de>
2015-05-05 17:05:22 +0200

    artikeldialogshit

---
+++ UEB02/UEB02/Artikeldialog.cpp
@@ -0,0 +1,36 @@
+/*
+ * Artikeldialog.cpp
+ *
+ *  Created on: 05.05.2015
+ *      Author: sbastian
+ */
+
+#include "Artikeldialog.h"
+
+Artikeldialog::Artikeldialog() {
+	// Nothing to do here
+}
+
+Artikeldialog::~Artikeldialog() {
+	// Nothing to do here
+}
+void Artikeldialog::testeBucheAbgang(){
+	// TODO implement this
+}
+
+void Artikeldialog::testeBucheZugang(){
+	// TODO implement this
+}
+
+void Artikeldialog::testeSetArtikelNr(){
+	// TODO implement this
+}
+
+void Artikeldialog::testeSetBestand(){
+	// TODO implement this
+}
+void Artikeldialog::testeSetBezeichnung(){
+	// TODO implement this
+}
+
+
---
+++ UEB02/UEB02/Artikeldialog.h
@@ -0,0 +1,24 @@
+/*
+ * Artikeldialog.h
+ *
+ *  Created on: 05.05.2015
+ *      Author: sbastian
+ */
+
+#ifndef ARTIKELDIALOG_H_
+#define ARTIKELDIALOG_H_
+
+class Artikeldialog {
+public:
+	Artikeldialog();
+	virtual ~Artikeldialog();
+	void testeBucheZugang();
+	void testeBucheAbgang();
+	void testeSetArtikelNr();
+	void testeSetBezeichnung();
+	void testeSetBestand();
+	void testeAlles();
+};
+
+#endif /* ARTIKELDIALOG_H_ */
+
