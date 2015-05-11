9f97996a9a47c8fa07fcbbdda507af3f9effc48a
Simon Bastian <s9sibast@stud.uni-saarland.de>
2015-05-11 16:13:04 +0200

    doku fix "biref" -> "brief"

--- UEB02/UEB02/Artikeldialog.h
+++ UEB02/UEB02/Artikeldialog.h
@@ -17,58 +17,58 @@ public:
 	Artikeldialog();
 	virtual ~Artikeldialog();
 	/**
-	 * @biref Test des Konstruktors mit zwei Argumenten
+	 * @brief Test des Konstruktors mit zwei Argumenten
 	 * @param artikelNr
 	 * @param bezeichnung
 	 */
 	void testeConstructor2(int artikelNr, string bezeichnung);
 	/**
-	 * @biref Test des Konstruktors mit drei Argumenten
+	 * @brief Test des Konstruktors mit drei Argumenten
 	 * @param artikelNr
 	 * @param bezeichnung
 	 * @param bestand
 	 */
 	void testeConstructor3(int artikelNr, string bezeichnung, int bestand);
 	/**
-	 * @biref Test der Zugangsbuchung eines Artikels
+	 * @brief Test der Zugangsbuchung eines Artikels
 	 * @param artikel
 	 */
 	void testeBucheZugang(Artikel artikel);
 	/**
-	 * @biref Test der Abgangsbuchung eines Artikels
+	 * @brief Test der Abgangsbuchung eines Artikels
 	 * @param artikel
 	 */
 	void testeBucheAbgang(Artikel artikel);
 	/**
-	* @biref Test des Setzens der Bezeichnung eines Artikels
+	* @brief Test des Setzens der Bezeichnung eines Artikels
 	* @param artikel
 	*/
 	void testeSetBezeichnung(Artikel artikel);
 	/**
-	 * @biref Test des Setzens des Bestands eines Artikels
+	 * @brief Test des Setzens des Bestands eines Artikels
 	 * @param artikel
 	 */
 	void testeSetBestand(Artikel artikel);
 	/**
-	 * @biref Ausführung aller Tests
+	 * @brief Ausführung aller Tests
 	 */
 	void testeAlles();
 	/**
-	 * @birefDialog zur Auswahl zwischen automatischen Tests und manuellem Testen
+	 * @briefDialog zur Auswahl zwischen automatischen Tests und manuellem Testen
 	 */
 	void dialog();
 	/**
-	 * @biref Interaktiver Test eines Artikels
+	 * @brief Interaktiver Test eines Artikels
 	 * @param artikel
 	 */
 	void manuell(Artikel artikel);
 	/**
-	 * @biref Interaktives Erstellen eines Testartikels mit Bestand
+	 * @brief Interaktives Erstellen eines Testartikels mit Bestand
 	 * @details ruft manuell() auf
 	 */
 	void createArtikelComplete();
 	/**
-	 * @biref Interaktives Erstellen eines Testartikels ohne Bestand
+	 * @brief Interaktives Erstellen eines Testartikels ohne Bestand
 	 * @details ruft manuell() auf
 	 */
 	void createArtikelTwoParam();
