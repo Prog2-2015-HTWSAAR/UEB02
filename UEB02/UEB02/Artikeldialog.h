cca4df87d991e6a603013adbd296ee57e4023919
A1259 <schreinerandreas1@gmx.net>
2015-05-11 15:32:39 +0200

    no message

--- UEB02/UEB02/Artikeldialog.h
+++ UEB02/UEB02/Artikeldialog.h
@@ -1,10 +1,14 @@
-/*
- * Artikeldialog.h
- *
- *  Created on: 05.05.2015
- *      Author: sbastian
- */
-
+/**
+* compile: g++ -c -Wall -pedantic *.cpp
+* compile: g++ -o ueb01 *.o
+* @file Artikeldialog.h
+* @Author Andreas Schreiner & Simon Bastian
+*
+* @date 08.05.2015
+*
+* H file
+*
+*/
 #ifndef ARTIKELDIALOG_H_
 #define ARTIKELDIALOG_H_
 #include "Artikel.h"
@@ -14,59 +18,59 @@ public:
 	Artikeldialog();
 	virtual ~Artikeldialog();
 	/**
-	 * Test des Konstruktors mit zwei Argumenten
+	 * @biref Test des Konstruktors mit zwei Argumenten
 	 * @param artikelNr
 	 * @param bezeichnung
 	 */
 	void testeConstructor2(int artikelNr, string bezeichnung);
 	/**
-	 * Test des Konstruktors mit drei Argumenten
+	 * @biref Test des Konstruktors mit drei Argumenten
 	 * @param artikelNr
 	 * @param bezeichnung
 	 * @param bestand
 	 */
 	void testeConstructor3(int artikelNr, string bezeichnung, int bestand);
 	/**
-	 * Test der Zugangsbuchung eines Artikels
+	 * @biref Test der Zugangsbuchung eines Artikels
 	 * @param artikel
 	 */
 	void testeBucheZugang(Artikel artikel);
 	/**
-	 * Test der Abgangsbuchung eines Artikels
+	 * @biref Test der Abgangsbuchung eines Artikels
 	 * @param artikel
 	 */
 	void testeBucheAbgang(Artikel artikel);
-	/** remove?
-	 * Test des Setzens der Artikelnummer eines Artikels
-	 *
-	 */
+	/**
+	* @biref Test des Setzens der Bezeichnung eines Artikels
+	* @param artikel
+	*/
 	void testeSetBezeichnung(Artikel artikel);
 	/**
-	 * Test des Setzens des Bestands eines Artikels
+	 * @biref Test des Setzens des Bestands eines Artikels
 	 * @param artikel
 	 */
 	void testeSetBestand(Artikel artikel);
 	/**
-	 * Ausführung aller Tests
+	 * @biref Ausführung aller Tests
 	 */
 	void testeAlles();
 	/**
-	 * Dialog zur Auswahl zwischen automatischen Tests und manuellem Testen
+	 * @birefDialog zur Auswahl zwischen automatischen Tests und manuellem Testen
 	 */
 	void dialog();
 	/**
-	 * Interaktiver Test eines Artikels
+	 * @biref Interaktiver Test eines Artikels
 	 * @param artikel
 	 */
 	void manuell(Artikel artikel);
 	/**
-	 * Interaktives Erstellen eines Testartikels mit Bestand
-	 * ruft manuell() auf
+	 * @biref Interaktives Erstellen eines Testartikels mit Bestand
+	 * @details ruft manuell() auf
 	 */
 	void createArtikelComplete();
 	/**
-	 * Interaktives Erstellen eines Testartikels ohne Bestand
-	 * ruft manuell() auf
+	 * @biref Interaktives Erstellen eines Testartikels ohne Bestand
+	 * @details ruft manuell() auf
 	 */
 	void createArtikelTwoParam();
 };
